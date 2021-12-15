b9727bc244a32c26b7e40c5f0b112348ed022aa1
ck0709 <96092327+ck0709@users.noreply.github.com>
2021-12-15 18:21:33 +0900

    Add files via upload

--- js/문제/q020/q020.js
+++ js/문제/q020/q020.js
@@ -2,11 +2,11 @@ const star="☆";
 //const star="*";
 const space="　";
 for( i = 1 ; i <= 10 ; i = i + 1 ) {
-    for ( j = 1 ; j <= 11 ; j++ ) {
-        if(i>(j-1)){
-            document.write(space);
-        }else {
+    for ( j = 1 ; j <= 10 ; j++ ) {
+        if(j>(i-1)){
             document.write(star);
+        }else {
+            document.write(space);
         }
     }
 document.write("<br>")
