e1eb3235a570890e0523192380ca9a6d9ade3f5a
ck0709 <96092327+ck0709@users.noreply.github.com>
2021-12-16 15:42:59 +0900

    Add files via upload

---
+++ js/문제/q027/common.js
@@ -0,0 +1,7 @@
+/*
+     공통함수를 모아놓음
+*/
+function dw(string){          // var string;  var string = " 고양이 ";
+    document.write(string);
+}
+dw("고양이");
---
+++ js/문제/q027/q027.js
@@ -0,0 +1,16 @@
+function popup(name, age, type){
+
+    catAge=5;
+    catType="페르시아";
+    catName="흰둥이";
+
+    var catAll="우리집 애완동물 이름은 " +name+
+     " 입니다.\n우리집 애완동물 나이는 "+age+
+      "살 입니다.\n우리집 애완동물 종류는 " +type+
+      " 입니다";
+
+    alert(catAll);
+}
+popup("두부",7,"포메라니안");
+popup("콩이",2,"포메라니안");
+popup("비지",1,"포메라니안");
