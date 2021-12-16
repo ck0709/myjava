2645b74ca17fc629e88fb04da2dfe6d7bb0f522d
ck0709 <96092327+ck0709@users.noreply.github.com>
2021-12-16 14:36:24 +0900

    Add files via upload

--- js/문제/q022/q022.js
+++ js/문제/q022/q022.js
@@ -1,11 +1,11 @@
+var catAge;
+var catType;
+var catName;
 
-var cat_name = prompt("고양이 이름");
-var cat_age = prompt("고양이 나이");
-var cat_kind = prompt("고양이 품종");
+catAge=5;
+catType="페르시아"
+catName="흰둥이"
 
-document.write(cat_name+"<br>"+cat_age+"<br>"+cat_kind);
+var catAll="고양이 이름은"+catName+"입니다.\n 고양이 종류는"+catType+"입니다.\n고양이 나이는"+catAge+"입니다.";
 
-
-
-
-
+alert(catAll);
